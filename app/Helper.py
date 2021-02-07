0dcb3fd4c0e188c7a662acd29e1ccc0fb7a6c75e
Benjamin Kauffmann <benjaminkauffmann@hotmail.fr>
2021-02-07 17:06:44 +0100

    second commit

---
+++ app/Helper.py
@@ -0,0 +1,15 @@
+class Helper:
+
+    """ 
+        Binarise une image à niveaux de gris à partir d’un critère d’entropie floue
+
+        Parameters
+        ----------
+    
+        Returns
+        -------
+
+        """
+    @staticmethod
+    def binarisation():
+        pass
